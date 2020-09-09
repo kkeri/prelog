39d29a4fd65218466946a47c84cb82f1a798c775
Kalman Keri <kk@metamaya.org>
2020-09-09 22:28:28 +0200

    Add native process that prints the program

--- src/interpreter/native.ts
+++ src/interpreter/native.ts
@@ -1,10 +1,12 @@
+import { DummyOutputStream } from "../stream"
 import * as syntax from "../syntax"
+import { OutputStream } from "../types"
 import { Environment } from "./environment"
-import { evaluate, resolve, success, lowerMeet } from "./interpreter"
-import { Definition, Model, Name, NativeProcess, Process, SyntaxError } from "./model"
+import { evaluate, lowerMeet, resolve, success } from "./interpreter"
+import { And, Definition, Model, Name, NativeProcess, Process, SyntaxError } from "./model"
 
 export function getNativeEnvironment (): Environment {
-  const env = new Environment(success)
+  const env = new Environment(success, {}, {})
   for (const name in nativeProcesses) {
     env.program = lowerMeet(env, env.program, (new Definition(
       new Name(name),
@@ -28,4 +30,23 @@ const nativeProcesses = {
   proc (env: Environment): Model {
     return new Process(env.next())
   },
+
+  '@env' (env: Environment): Model {
+    printEnvironment(env, env.outputs.stdout ?? new DummyOutputStream())
+    return success
+  }
+}
+
+export function printEnvironment (env: Environment, out: OutputStream) {
+  _printEnvironment(env, out, env.program)
+}
+
+export function _printEnvironment (env: Environment, out: OutputStream, model: Model) {
+  if (model instanceof And) {
+    _printEnvironment(env, out, model.a)
+    _printEnvironment(env, out, model.b)
+  }
+  else {
+    out.write(model.reflect())
+  }
 }
