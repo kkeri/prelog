75e199d2267b6859e36df4ef498c010692a7c48a
Kalman Keri <kk@metamaya.org>
2020-09-10 23:22:16 +0200

    Add more native definitions

--- src/interpreter/native.ts
+++ src/interpreter/native.ts
@@ -1,25 +1,38 @@
 import { DummyOutputStream } from "../stream"
 import * as syntax from "../syntax"
 import { OutputStream } from "../types"
+import { Dictionary } from "../util/types"
 import { Environment } from "./environment"
 import { evaluate, lowerMeet, resolve, success } from "./interpreter"
-import { And, Definition, Model, Name, NativeProcess, Process, SyntaxError, ParentEnvironment } from "./model"
+import { And, Definition, Model, NativeProcess, ParentEnvironment, Process, SyntaxError, Atom, Num } from "./model"
+import { Rank } from "./threshold"
 
 export function getNativeEnvironment (): Environment {
   const env = new Environment(success, {}, {})
-  for (const name in nativeProcesses) {
-    env.program = lowerMeet(env, env.program, () => new Definition(
-      new syntax.Name(name),
-      new NativeProcess(
-        new syntax.Name(name),
-        nativeProcesses[name]
-      )
-    ))
+  for (const name in nativeDefs) {
+    env.program = lowerMeet(
+      env,
+      env.program,
+      () => translateNativeDef(name, nativeDefs[name]),
+    )
   }
   return env
 }
 
-const nativeProcesses = {
+type NativeDefinition = Model | ((env: Environment) => Model)
+
+function translateNativeDef (name: string, value: NativeDefinition): Model {
+  const n = new syntax.Name(name)
+  const v = typeof value === 'function'
+    ? new NativeProcess(n, value)
+    : value
+  return new Definition(n, v)
+}
+
+const nativeDefs: Dictionary<NativeDefinition> = {
+  true: new Atom(Rank.True, new syntax.Name('true')),
+  false: new Atom(Rank.False, new syntax.Name('false')),
+
   def (env: Environment): Model {
     const name = env.next()
     if (!(name instanceof syntax.Name)) return new SyntaxError('the first argument of a definition must be a name', name)
@@ -31,10 +44,14 @@ const nativeProcesses = {
     return new Process(env.next())
   },
 
+  rank (env: Environment): Model {
+    return new Num(evaluate(env, env.next()).rank)
+  },
+
   '@env' (env: Environment): Model {
     printEnvironment(env.program, env.outputs.stdout ?? new DummyOutputStream())
     return success
-  }
+  },
 }
 
 export function printEnvironment (m: Model, out: OutputStream) {
